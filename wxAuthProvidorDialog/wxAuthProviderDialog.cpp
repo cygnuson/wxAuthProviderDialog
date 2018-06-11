274524df62135ef77292241134264c590bbd912a
Cygnus <cygnusvis@gmail.com>
2018-06-10 22:06:08 -0700

    added a license

--- wxAuthProvidorDialog/wxAuthProviderDialog.cpp
+++ wxAuthProvidorDialog/wxAuthProviderDialog.cpp
@@ -1,4 +1,23 @@
+/*
 
+(C) Matthew Swanson
+
+This file is part of wxAuthProviderDialog.
+
+wxAuthProviderDialog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+wxAuthProviderDialog is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with wxAuthProviderDialog.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #include <fstream>
 #include <thread>
 
